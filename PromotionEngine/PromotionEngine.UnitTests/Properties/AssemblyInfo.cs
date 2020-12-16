b4972b48cf95966cfdb07b7a08763011253d7143
S, Saurabh <saurabh_s@baxter.com>
2020-12-16 11:39:05 +0530

    first commit

---
+++ PromotionEngine/PromotionEngine.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PromotionEngine.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PromotionEngine.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b143e360-ac16-4e50-a1d6-6c8463b33a1e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ PromotionEngine/PromotionEngine.UnitTests/UnitTest1.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using PromotionEngine;
+
+namespace PromotionEngine.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        PromotionChecker.PromotionEngine promotionChecker;
+       [TestMethod]
+        public void TestMethod1()
+        {
+            PromotionChecker promotionChecker = new PromotionChecker();
+            // Act
+            try
+            {
+                List<Order> orders = new List<Order>();
+                Order order1 = new Order(1, new List<Product>() { new Product("A"), new Product("A"), new Product("B"), new Product("B"), new Product("C"), new Product("D") });
+                List<Promotion> promotions = new List<Promotion>()
+                {
+                     new Promotion(1, d1, 130),
+                     new Promotion(2, d2, 45),
+                     new Promotion(3, d3, 30)
+                 };
+                promotionChecker.GetTotalPrice(order1, promotions);
+                Assert.AreEqual("Orders and the products", request.Status);
+            }
+            catch (System.ArgumentOutOfRangeException e)
+            {
+                // Assert
+                StringAssert.Contains(e.Message, UnitTest1.TestMethod1);
+                return;
+            }
+
+            Assert.Fail("The expected exception was not thrown.");
+
+        }
+    }
+}
